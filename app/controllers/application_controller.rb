997a53f247dd910008c6bd35f491cda182ef2658
Ruslan <ruslanrally@gmail.com>
2020-01-28 14:06:25 +0200

    API-KEY allowed in headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::API
   def set_access_control_headers
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Allow-Headers'] = 'API-KEY, Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
     response.headers['Access-Control-Max-Age'] = '1728000'
   end
 
