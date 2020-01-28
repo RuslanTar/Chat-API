9c2e049c417aa136f690cc8feefc49e3df74b6c5
Ruslan <ruslanrally@gmail.com>
2020-01-28 12:04:59 +0200

    little changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :users, param: :_name
   post '/auth/login'       =>'authentication#login'
   get '/*a'                =>'application#not_found'
+
   #User actions
-  get    '/users'          => 'users#index'
-  post   '/users/create'   => 'users#create'
-  patch  '/user/:id'       => 'users#update'
-  delete '/user/:id'       => 'users#destroy'
+  get    '/auth/me'           => 'users#current'
+  get    '/users'             => 'users#index'
+  post   '/users/create'      => 'users#create'
+  patch  '/user/update'       => 'users#update'
+  delete '/user/delete'       => 'users#destroy'
 end
