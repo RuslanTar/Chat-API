871a80e95a7296f8cbc31b614cb89994dad4fd46
Ruslan <ruslanrally@gmail.com>
2020-01-28 12:11:26 +0200

    OPTION request handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::API
 
+  after_filter :set_access_control_headers
+
   def not_found
     render json: { error: 'not_found' }
   end
 
+  def handle_options_request
+    head(:ok) if request.request_method == "OPTIONS"
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+  end
 
   def authorize_request
     header = request.headers['Authorization']
