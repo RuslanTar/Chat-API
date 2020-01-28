849e41f234a028484696584d29e31d14500fdab1
Ruslan <ruslanrally@gmail.com>
2020-01-28 18:10:09 +0200

    debug current (try#4)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, param: :_name
   match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
   post '/auth/login'       =>'authentication#login'
-  get '/*a'                =>'application#not_found'
+  # get '/*a'                =>'application#not_found'
 
   #User actions
   get    '/auth/me'           => 'users#current'
