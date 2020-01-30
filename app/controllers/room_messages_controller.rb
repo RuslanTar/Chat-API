3443aa12d83d9ffae9c80aca355c83461fc16eb2
LVALL <valeradracenko418@gmail.com>
2020-01-30 12:52:02 +0200

    rooms test #1
    Described rooms_controller and room_messages_controller

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -1,2 +1,16 @@
 class RoomMessagesController < ApplicationController
+  before_action :load_entities
+  before_action :find_user, except: %i[create index]
+
+  def create
+    @room_message = RoomMessage.create user: @user,
+                                       room: @room,
+                                       message: params.dig(:room_message, :message)
+  end
+
+  protected
+
+  def load_entities
+    @room = Room.find params.dig(:room_message, :room_id)
+  end
 end
