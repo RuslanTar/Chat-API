3fe9eb11d21462cd108f476366d84bcee43429bb
Ruslan <ruslanrally@gmail.com>
2020-02-03 12:23:50 +0200

    add refresh path

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # get '/*a'                =>'application#not_found'
 
   #User actions
+  post   '/refresh'           => 'users#refresh'
   get    '/profile'           => 'users#current_profile'
   get    '/profile/:id'       => 'users#profile'
   get    '/auth/me'           => 'users#current'
