e63acc7a86dcb19dbdfdb571d4b3c2749b9dd8e2
LVALL <valeradracenko418@gmail.com>
2020-02-08 21:39:25 +0200

    debag destroy user#6

--- app/models/assigned_user.rb
+++ app/models/assigned_user.rb
@@ -1,6 +1,6 @@
 class AssignedUser < ApplicationRecord
   belongs_to :room
-  belongs_to :user, dependent: :destroy
+  belongs_to :user#, dependent: :destroy
 
   validates :user, uniqueness: { scope: :room }
 end
